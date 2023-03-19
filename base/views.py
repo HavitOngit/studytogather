56b7ed113a1ee49e09478725fe197b178d161abc
Havit <havitonline@gmail.com>
2023-03-19 23:14:25 +0530

    query topics

--- base/views.py
+++ base/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Rooms
+from .models import Rooms, Topic
 from .form import RoomForm
 # Create your views here.
 
 
-rooms = [
-    {'id':1, 'name':'Python Class'},
-    {'id':2, 'name':'maths Class'},
-    {'id':3, 'name':'pysology Class'}
-]
+
 def home(request):
-    rooms = Rooms.objects.all()
-    return render(request, 'home.html', {'rooms':rooms})
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Rooms.objects.filter(topic__name__icontains=q)
+
+    topic = Topic.objects.all()
+
+    return render(request, 'home.html', {'rooms':rooms, 'topics':topic})
 
 def room(request, pk):
+
     context = Rooms.objects.get(id=pk)
     return render(request, 'room.html', {'room':context})
+
 
 def createRoom(request):
     form = RoomForm()
