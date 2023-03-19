61316ef72beeaabb9a0c6bf60b11233d39c8813a
Havit <havitonline@gmail.com>
2023-03-19 20:41:04 +0530

    ordering room list

--- base/models.py
+++ base/models.py
@@ -17,6 +17,9 @@ class Rooms(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
