9f7a98e10ffa43c8ae7b6b049804f06be7c38ba8
Havit <havitonline@gmail.com>
2023-04-02 00:41:57 +0530

    almost dne

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
 from .models import Rooms, Topic, Massage, User
-from .form import RoomForm, UserForm
+from .form import RoomForm, UserForm, MyUserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.http import HttpResponse
@@ -51,10 +51,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerUser(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -72,7 +72,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES,instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
